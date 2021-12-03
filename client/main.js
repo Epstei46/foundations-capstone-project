7b95f3caf529e1f9e2791618375c9a2b937737b5
Steven Epstein <sbepstein42@gmail.com>
2021-12-03 10:22:55 -0500

    Reverted baseURL pre-testing

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 const listContainer = document.querySelector("#list-container")
 const form = document.querySelector("form")
 
-const baseURL = `/api/shows`
+const baseURL = `${window.location.origin}/api/shows`
 
 const showsCallback = ({ data: shows}) => displayShows(shows)
 const errCallback = err => console.log(err)
