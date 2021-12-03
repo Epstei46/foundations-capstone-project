64b8c128cb9facecf5586a0d70562e728de706e7
Steven Epstein <sbepstein42@gmail.com>
2021-12-03 11:50:45 -0500

    implemented 'path' instead of using clever code

--- server/index.js
+++ server/index.js
@@ -2,26 +2,15 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const ctrl = require("./controller")
+const path = require('path');
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static(`client`))
 
-const { dirname } = require('path');
-let appDir = dirname(require.main.filename);
-// appDir = __dirname. Issue is that the root folder when testing on localserver is the server folder, so below, I am removing "server" from the root folder string. If I try to res.sendfile("../client/main.html") then I get a forbidden error because I am trying to go up/back a folder, not allowed in the browser. Alternatively, could have used path.join which would have allowed the "/../"
-if (appDir.includes("server")){
-    appDir = appDir.substring(0,appDir.length-7)}
-
 app.get("/", function (req, res) {
-    res.sendFile(appDir + "/client/main.html");
+    res.sendFile(path.join(__dirname, '../client/main.html'));
 });
-// app.get("/styles", (req, res) => {
-//     res.sendFile(path.join(__dirname, "/../client/styles.css"));
-// });
-// app.get("/js", (req, res) => {
-//     res.sendFile(path.join(__dirname, "/../client/main.js"));
-// });
 
 app.get("/api/shows", ctrl.getAllShows)
 app.post("/api/shows", ctrl.addShow)
@@ -38,3 +27,13 @@ app.listen(port, () => {
 // const production = "https://simple-watch-list.herokuapp.com/api/shows";
 // const development = "http://localhost:4242/api/shows";
 // const baseURL = (process.env.PATH ? production : development);
+
+
+// const { dirname } = require('path');
+// let appDir = dirname(require.main.filename);
+// /* appDir = __dirname. Issue is that the root folder when testing on localserver is the server folder, so below, I am removing "server" from the root folder string. If I try to res.sendfile("../client/main.html") then I get a forbidden error because I am trying to go up/back a folder, not allowed in the browser. Alternatively, could have used path.join which would have allowed the "/../" */
+// if (appDir.includes("server")){
+//     appDir = appDir.substring(0,appDir.length-7)}
+// app.get("/", function (req, res) {
+//     res.sendFile(appDir + "/client/main.html");
+// });
